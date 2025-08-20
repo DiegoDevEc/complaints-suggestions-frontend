24376efb0af27ad5bb56428705051bbe0d554597
DiegoDevEc <diego1503bsc@gmail.com>
2025-08-20 00:27:53 -0500

    feat: add status dropdown for complaints

--- src/app/pages/service/complaints.service.ts
+++ src/app/pages/service/complaints.service.ts
@@ -2,36 +2,35 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
-export type FeedbackStatus = 'PENDING' | 'CANCEL';
+export type FeedbackStatus = 'PENDING' | 'RESOLVED' | 'IN_PROGRESS';
 export type FeedbackType = 'complaint' | 'compliment' | 'suggestion';
 
 export interface Feedback {
-  _id: string;
-  status: FeedbackStatus;
-  lastName: string;
-  firstName: string;
-  email: string;
-  description: string;
-  phone: string;
-  type: FeedbackType;
-  contacted: boolean;
-  latitude: number;
-  longitude: number;
-  dateRegister: string; // ISO 8601 string
-  __v: number;
+    _id: string;
+    status: FeedbackStatus;
+    lastName: string;
+    firstName: string;
+    email: string;
+    description: string;
+    phone: string;
+    type: FeedbackType;
+    contacted: boolean;
+    latitude: number;
+    longitude: number;
+    dateRegister: string; // ISO 8601 string
+    __v: number;
 }
 
 export interface FeedbackListResponse {
-  data: Feedback[];
-  total: number;
-  page: number;
-  limit: number;
+    data: Feedback[];
+    total: number;
+    page: number;
+    limit: number;
 }
 
 @Injectable()
 export class ComplaintsService {
-
-    private readonly apiUrl = environment.backendUrl
+    private readonly apiUrl = environment.backendUrl;
 
     constructor(private http: HttpClient) {}
 
@@ -43,5 +42,4 @@ export class ComplaintsService {
         console.log('Cancelling feedback with ID:', feedbackId);
         return this.http.delete(`${this.apiUrl}/private/feedback/${feedbackId}`);
     }
-
 }
