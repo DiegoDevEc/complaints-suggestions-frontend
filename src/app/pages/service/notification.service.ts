93d6133fd87b0b89578e1ef13c46856170a50466
Diego Acosta <diego.acosta@externos.bci.cl>
2025-09-29 02:35:16 -0500

    Ajustes deploy

--- src/app/pages/service/notification.service.ts
+++ src/app/pages/service/notification.service.ts
@@ -5,7 +5,7 @@ import { io } from 'socket.io-client';
 @Injectable({ providedIn: 'root' })
 export class NotificationsService {
 
-    private socket = io('http://hckk8oogsckgggc08w4wgg4k.31.97.12.29.sslip.io', {
+    private socket = io('https://api.app-escom.org', {
         transports: ['websocket'],
         reconnection: true,
     });
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    backendUrl: 'http://hckk8oogsckgggc08w4wgg4k.31.97.12.29.sslip.io',
+    backendUrl: 'https://api.app-escom.org',
     googleMapsApiKey: 'AIzaSyCYk6P2-xjdbZcUQI15gPCUsKUhnrkjXSs'
 };
